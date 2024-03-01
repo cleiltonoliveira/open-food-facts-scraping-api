63a5c68c25ec63368d02cb09251d7e2b9a950a69
cleiltonoliveira <clltnoliveira@gmail.com>
2024-03-01 10:52:15 -0300

    Add Swagger to application routes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { setupSwagger } from './swagger/swagger-setup';
+
 async function bootstrap() {
   const envFile = process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev';
-  config({ path: envFile }); 
+  config({ path: envFile });
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
