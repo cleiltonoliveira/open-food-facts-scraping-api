1efc5d4f271474472ff54f13ce7fa33455ff5c6c
cleiltonoliveira <clltnoliveira@gmail.com>
2024-03-01 00:32:24 -0300

    Environment type parameterized at startup

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 async function bootstrap() {
-  config();
+  const envFile = process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev';
+  config({ path: envFile }); 
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
 }
